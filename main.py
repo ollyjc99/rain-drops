7b23dba66953bb1bd217e7708656d695abac63df
Oliver Carter <oliverc101@hotmail.com>
2020-08-16 14:50:53 +0100

    updated cloud gen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from random import randint
+import time
+from threading import Thread
 
 
 class Ground(pygame.sprite.Sprite):
@@ -9,8 +11,18 @@ class Ground(pygame.sprite.Sprite):
         self.image = pygame.Surface((win.get_width(), 40))
         self.image.fill((50, 150, 50))
         self.rect = self.image.get_rect()
-        print(h)
         self.rect.center = (w // 2, h-20)
+
+
+class Rain(pygame.sprite.Sprite):
+    def __init__(self, win):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((8, 8))
+        self.rect = self.image.get_rect()
+        self.win = win
+
+    def draw(self):
+        pygame.draw.ellipse(self.win, (0,25,200), self.rect)
 
 
 class Cloud(pygame.sprite.Sprite):
@@ -19,18 +31,43 @@ class Cloud(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale((pygame.image.load('img/cloud.png')), (180, 100))
         self.rect = self.image.get_rect()
-        self.rect.centerx = randint(self.image.get_width(), w-self.image.get_width())
+        self.rect.centerx = randint(0, w)
+        self.lifespan = randint(600, 600*10)
+
+    def update(self):
+        self.lifespan -= 1
+        if self.lifespan == 0:
+            self.kill()
+
+
+class CloudGen(Thread):
+    def __init__(self, win, clouds):
+        Thread.__init__(self)
+        self.win = win
+        self.clouds = clouds
+        self.deamon = True
+        self.start()
+
+    def run(self):
+        while True:
+            r = randint(0, 1000)
+            if r > 750:
+                cloud = Cloud(self.win)
+                self.clouds.add(cloud)
+            time.sleep(1)
 
 
 def main():
     win = pygame.display.set_mode((800, 600))
 
     ground = Ground(win)
-    cloud = Cloud(win)
     all_ground = pygame.sprite.Group()
-    clouds = pygame.sprite.Group()
     all_ground.add(ground)
-    clouds.add(cloud)
+
+    all_rain = pygame.sprite.Group()
+
+    clouds = pygame.sprite.Group()
+    CloudGen(win, clouds)
 
     running = True
     while running:
@@ -42,10 +79,15 @@ def main():
         # Game Render
         win.fill((122, 160, 147))
         all_ground.draw(win)
+        clouds.update()
         clouds.draw(win)
         pygame.display.update()
 
 
 if __name__ == '__main__':
+    time_start = time.perf_counter()
     pygame.init()
     main()
+    pygame.quit()
+    time_finish = time.perf_counter()
+    print(f'Finished in {round(time_finish - time_start, 3)} second(s)')
