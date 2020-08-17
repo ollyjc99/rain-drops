cd9271aba4c8c80ba2f1f2609f775ce34eb46fc0
Oliver Carter <oliverc101@hotmail.com>
2020-08-17 17:44:18 +0100

    added tree generation

--- trees.py
+++ trees.py
@@ -1,19 +1,24 @@
 import pygame
+from random import randint
 from threading import Thread
 
 
 class Tree(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, pos):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('img/pine-tree.png')
-        self.rect = self.image.get_rect()
+        self.image = pygame.transform.scale(pygame.image.load('img/pine-tree.png'), (randint(280, 350), randint(320, 420)))
+        x, y = pos
+        self.rect = self.image.get_rect(centerx=x, bottom=y)
 
 
 class TreeGen(Thread):
-    def __init__(self):
+    def __init__(self, ground, trees):
         Thread.__init__(self)
         self.daemon = True
+        self.ground = ground
+        self.trees = trees
         self.start()
 
     def run(self):
-        pass
+        for i in range(randint(25, 75)):
+            self.trees.add(Tree((randint(self.ground.rect.x, self.ground.rect.width), self.ground.rect.top)))
