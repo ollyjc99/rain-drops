feeb9321a913368fdd4c2f2b0da4ccf7447d72fc
Oliver Carter <oliverc101@hotmail.com>
2020-08-17 14:56:30 +0100

    started tree thread

--- trees.py
+++ trees.py
@@ -4,5 +4,16 @@ from threading import Thread
 
 class Tree(pygame.sprite.Sprite):
     def __init__(self):
-        pygame.sprite.Sprite(self)
+        pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('img/pine-tree.png')
+        self.rect = self.image.get_rect()
+
+
+class TreeGen(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        pass
