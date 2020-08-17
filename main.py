f35d6d0c811b2b5b5212096358e3837353663bac
Oliver Carter <oliverc101@hotmail.com>
2020-08-17 14:11:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from random import randint
 import time
 from threading import Thread
 import atexit
+from clouds import *
 
 
 class Ground(pygame.sprite.Sprite):
@@ -13,98 +14,6 @@ class Ground(pygame.sprite.Sprite):
         self.image.fill((50, 150, 50))
         self.rect = self.image.get_rect()
         self.rect.center = (w // 2, h-20)
-
-
-class Rain(pygame.sprite.Sprite):
-    def __init__(self, win, rect):
-        pygame.sprite.Sprite.__init__(self)
-        self.win = win
-        self.image = pygame.Surface((5,5))
-        self.image.fill((0,100,225))
-        self.cloud = rect
-        self.rect = self.image.get_rect(centerx=randint(self.cloud.x, self.cloud.x+self.cloud.width), bottom=self.cloud.bottom)
-
-    def update(self, win, ground, splash):
-        self.rect.y += 3
-        if pygame.sprite.spritecollideany(self, ground):
-            for pos in splashfx((self.rect.x, self.rect.y)):
-                SplashGen(win, pos, splash)
-            self.kill()
-
-
-class Splash(pygame.sprite.Sprite):
-    def __init__(self, win, pos):
-        pygame.sprite.Sprite.__init__(self)
-        self.win = win
-        self.lifespan = 5
-        self.image = pygame.Surface((3, 3))
-        self.image.fill((0,100,225))
-        self.rect = self.image.get_rect(center=pos)
-
-    def update(self):
-        self.lifespan -= 1
-        if self.lifespan <= 0:
-            self.kill()
-
-
-class Cloud(pygame.sprite.Sprite):
-    def __init__(self, win):
-        w, h = win.get_size()
-        pygame.sprite.Sprite.__init__(self)
-        self.win = win
-        self.image = pygame.transform.scale((pygame.image.load('img/cloud.png')), (180, 100))
-        self.rect = self.image.get_rect()
-        self.rect.centerx = randint(0, w)
-        self.lifespan = randint(600, 600*10)
-
-    def update(self, rain):
-        self.lifespan -= 1
-        if self.lifespan == 0:
-            self.kill()
-        if randint(6,30) > 25:
-            rain.add(Rain(self.win, self.rect))
-
-
-class CloudGen(Thread):
-    def __init__(self, win, clouds):
-        Thread.__init__(self)
-        self.win = win
-        self.clouds = clouds
-        self.running = True
-        self.start()
-
-    def run(self):
-        while self.running:
-            print('Running')
-            r = randint(0, 1000)
-            if r > 700:
-                cloud = Cloud(self.win)
-                self.clouds.add(cloud)
-            time.sleep(1)
-
-
-class SplashGen(Thread):
-    def __init__(self, win, pos, splash):
-        Thread.__init__(self)
-        self.daemon = True
-        self.win = win
-        self.pos = pos
-        self.splash = splash
-        self.start()
-
-    def run(self):
-        self.splash.add(Splash(self.win, self.pos))
-
-
-def splashfx(pos):
-    x, y = pos
-    return [
-        (x-4, y-1),
-        (x-1.5, y+1),
-        (x, y+1),
-        (x+1.5, y+1),
-        (x+4, y-1)
-    ]
 
 
 def main():
