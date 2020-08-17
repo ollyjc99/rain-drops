ab26944271e0a2dbbb071250940064b76ab5d4a5
Oliver Carter <oliverc101@hotmail.com>
2020-08-17 14:56:13 +0100

    moved ground class to separate py file

---
+++ ground.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, win):
+        w, h = win.get_size()
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((win.get_width(), 40))
+        self.image.fill((50, 150, 50))
+        self.rect = self.image.get_rect()
+        self.rect.center = (w // 2, h-20)
+
+
