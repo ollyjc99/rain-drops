14525fa87d5bb9ce456a508c62d0b49243e6dea0
Oliver Carter <oliverc101@hotmail.com>
2020-08-17 14:10:48 +0100

    Update rain.py

---
+++ rain.py
@@ -0,0 +1,20 @@
+import pygame
+from random import randint
+from splash import *
+
+
+class Rain(pygame.sprite.Sprite):
+    def __init__(self, win, rect):
+        pygame.sprite.Sprite.__init__(self)
+        self.win = win
+        self.image = pygame.Surface((5,5))
+        self.image.fill((0,100,225))
+        self.cloud = rect
+        self.rect = self.image.get_rect(centerx=randint(self.cloud.x, self.cloud.x+self.cloud.width), bottom=self.cloud.bottom)
+
+    def update(self, win, ground, splash):
+        self.rect.y += 3
+        if pygame.sprite.spritecollideany(self, ground):
+            for pos in splashfx((self.rect.x, self.rect.y)):
+                SplashGen(win, pos, splash)
+            self.kill()
