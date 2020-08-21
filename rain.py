334b9e1dd2db80a971a4365ce366da11bf766353
Oliver Carter <oliverc101@hotmail.com>
2020-08-21 15:18:54 +0100

    Update rain.py

--- rain.py
+++ rain.py
@@ -3,6 +3,20 @@ from random import randint
 from splash import *
 
 
+class DrawRain(Thread):
+    def __init__(self, *args):
+        Thread.__init__(self)
+        self.daemon = True
+        self.win, self.rain = args
+        self.start()
+
+    def run(self):
+        self.rain.draw(self.win)
+
+    def __str__(self):
+        return 'Process to draw rain sprites'
+
+
 class Rain(pygame.sprite.Sprite):
     def __init__(self, win, rect):
         pygame.sprite.Sprite.__init__(self)
